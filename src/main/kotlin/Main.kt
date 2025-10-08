3b664bee64297c2b8b28b790155c1d745d9d2f96
DiLilloDaniele <daniele.dilillo@studio.unibo.it>
2025-10-08 14:43:12 +0200

    chore. updated main startup function and application config file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 package org.example
 
 import it.wldt.core.engine.DigitalTwinEngine
+import org.example.com.smartassistantdrive.trafficdt.dt.AggregateTrafficDt
 import org.example.com.smartassistantdrive.trafficdt.dt.TrafficDT
 import org.example.com.smartassistantdrive.trafficdt.interfaceAdaptersLayer.webService.TrafficRouting
 
@@ -9,7 +10,8 @@ fun main() {
 	// TrafficRouting.execute()
 
 	val digitalTwinEngine = DigitalTwinEngine()
-	digitalTwinEngine.addDigitalTwin(TrafficDT("trafficdt", 0).getDigitalTwin())
+	digitalTwinEngine.addDigitalTwin(AggregateTrafficDt("aggregate-traffic").getDigitalTwin(), true)
+	digitalTwinEngine.addDigitalTwin(TrafficDT("trafficdt", 0, "roadId", 0, 2).getDigitalTwin())
 	// Start all the DTs registered on the engine
 	digitalTwinEngine.startAll()
 }
